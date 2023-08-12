e21b56c951619202bd87c98c016dd1b193e955b1
Rillis Vernucio <rillisnelson@gmail.com>
2023-08-12 01:00:12 -0300

    database remake

---
+++ sql/tables.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE IF NOT EXISTS includeplus;
+
+/* USER TABLE */
+DROP TABLE IF EXISTS includeplus.users;
+CREATE TABLE includeplus.users(
+                                id int AUTO_INCREMENT PRIMARY KEY,
+                                name varchar(255) NOT NULL,
+                                cpf int NOT NULL,
+                                password varchar(255) NOT NULL,
+                                role varchar(255) DEFAULT 'user',
+                                created_at timestamp not null
+                              );
+
+/* PLACES TABLE */
+DROP TABLE IF EXISTS includeplus.places;
+CREATE TABLE includeplus.places(
+                                id int AUTO_INCREMENT PRIMARY KEY,
+                                display_name varchar(255) NOT NULL,
+                                address varchar(255) NOT NULL,
+                                cep varchar(255) NOT NULL,
+                                latitude double not null,
+                                longitude double not null,
+                                created_by int NOT NULL,
+                                created_at timestamp not null
+                              );
+
+/* PLACE REQUESTS TABLE */
+DROP TABLE IF EXISTS includeplus.place_requests;
+CREATE TABLE includeplus.place_requests(
+                                id int AUTO_INCREMENT PRIMARY KEY,
+                                display_name varchar(255) NOT NULL,
+                                latitude double not null,
+                                longitude double not null,
+                                created_by int NOT NULL,
+                                created_at timestamp not null,
+                                create_post int not null
+                              );
+
+/* POSTS TABLE */
+DROP TABLE IF EXISTS includeplus.posts;
+CREATE TABLE includeplus.posts(
+                                id int AUTO_INCREMENT PRIMARY KEY,
+                                place_id INT,
+                                user_id INT NOT NULL,
+                                photo VARCHAR(255) NOT NULL,
+                                title VARCHAR(255) NOT NULL,
+                                approved_by int,
+                                dennied_by int,
+                                created_at timestamp not null
+                              );
+
+/* REMOVALS TABLE */
+DROP TABLE IF EXISTS includeplus.removals;
+CREATE TABLE includeplus.removals(
+                                id int AUTO_INCREMENT PRIMARY KEY,
+                                post_id INT not null,
+                                user_id INT NOT NULL,
+                                photo VARCHAR(255) NOT NULL,
+                                title VARCHAR(255) NOT NULL,
+                                approved_by int,
+                                dennied_by int,
+                                created_at timestamp not null
+                              );
---
+++ sql/views.sql
@@ -0,0 +1,17 @@
+drop view if exists includeplus.vw_active_posts;
+create view includeplus.vw_active_posts as
+select a.* from includeplus.posts a
+left join (select b.* from includeplus.removals b where b.approved_by is not null limit 1) c
+on a.id = c.post_id
+where a.approved_by is not null and c.approved_by is null;
+
+drop view if exists includeplus.vw_pending_posts;
+create view includeplus.vw_pending_posts as
+select a.* from includeplus.posts a
+where a.approved_by is null and dennied_by is null;
+
+drop view if exists includeplus.vw_pending_removals;
+create view includeplus.vw_pending_removals as
+select a.* from includeplus.removals a
+where a.approved_by is null and dennied_by is null;
+
